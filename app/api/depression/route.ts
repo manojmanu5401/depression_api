30efda506a98ede64835ab42d4e9c1393e420d4e
Manoj M <manojmanu5401@gmail.com>
2024-03-29 01:35:37 +0530

    project setup-01

---
+++ app/api/depression/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { loadModel, predict } from "@/lib/tf";
+
+export const POST = async(req: Request, res: Response) =>{
+    const {options} = await req.json();
+    try {
+        console.log("http://localhost:3000/model/depression/model.json");
+        await loadModel("http://localhost:3000/model/depression/model.json");
+        const result = await predict(options,0)
+        console.log("res:", result);
+        return NextResponse.json("result", {status: 200})
+    } catch (error) {
+        return NextResponse.json({messsage:error}, {status: 500})
+    }
+    console.log("POST")
+}
